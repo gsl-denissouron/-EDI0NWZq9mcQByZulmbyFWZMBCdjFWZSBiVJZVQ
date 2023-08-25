1c6638ddfd92a472de23f0ef4674b53288c86f19
Denis Souron <denis.souron-ext@groupeseloger.com>
2023-08-25 18:43:54 +0200

    Some ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,14 +6,38 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:i18next/recommended",
+    "plugin:import/typescript",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "i18next"],
+  plugins: ["react-refresh", "i18next", "import"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+    "import/order": [
+      "warn",
+      {
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "@domain/**",
+            group: "external",
+            position: "after",
+          },
+          {
+            pattern: "@infra/**",
+            group: "parent",
+            position: "before",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        pathGroupsExcludedImportTypes: ["builtin"],
+      },
     ],
   },
 };
